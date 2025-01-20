c583f2ac96fa3b852c133870723a894f4f1bf78d
Danil Gordeev <gordeevd477@gmail.com>
2025-01-20 16:18:42 +0900

    1 zadanie

---
+++ 321.js
@@ -0,0 +1,10 @@
+const myName = "Антон";
+const programmingLanguage = "JavaScript";
+const courseCreatorName = "Иван";
+const reasonText = "это перспективно и интересно";
+const numberOfMonth = 3;
+
+const resultText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе у ${courseCreatorName}.
+Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяца. Я уверен(а), что пройду данный курс до конца!`;
+
+console.log(resultText);
